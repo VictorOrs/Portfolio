0282873942e4d4150034b1af4c83deb0517181bb
Victor Oursin <victor.oursin@gmail.com>
2026-03-29 03:29:20 +0800

    Vercel Speed Insight Added

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,22 @@
 import { LoadingProvider } from "@/lib/loading";
 import LoadingScreen from "@/components/LoadingScreen";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
+  
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <LoadingProvider>
